09f46196b92145c574a3abb9fa13fcb63e84ad75
sken <22631338+sken77@users.noreply.github.com>
2020-04-05 19:24:38 -0700

    add CNumeric Laws (#4)
    * add CNumeric laws
    
    * add uint8
    
    * add not equal
    
    * refactor laws

--- core/src/main/scala/scash/warhorse/core/package.scala
+++ core/src/main/scala/scash/warhorse/core/package.scala
@@ -6,12 +6,24 @@ package object core extends SerdeSyntax with CNumericSyntax {
   import scala.{ Predef => P }
 
   implicit val bigIntNumeric: CNumeric[BigInt] =
-    CNumeric[BigInt](0xFFFFFFFFFFFFFFFFL)(P.identity, P.identity)
+    CNumeric[BigInt](
+      0xFFFFFFFFFFFFFFFFL,
+      BigInt(Long.MinValue),
+      BigInt(Long.MaxValue)
+    )(P.identity, P.identity)
 
   implicit val longNumeric: CNumeric[Long] =
-    CNumeric[Long](0xFFFFFFFFFFFFFFFFL)(P.identity[Long], _.toLong)
+    CNumeric[Long](
+      0xFFFFFFFFFFFFFFFFL,
+      Long.MinValue,
+      Long.MaxValue
+    )(P.identity[Long], _.toLong)
 
   implicit val intNumeric: CNumeric[Int] =
-    CNumeric[Int](0xFFFFFFFF)(P.identity[Int], _.toInt)
+    CNumeric[Int](
+      0xFFFFFFFF,
+      Int.MinValue,
+      Int.MaxValue
+    )(P.identity[Int], _.toInt)
 
 }
--- core/src/main/scala/scash/warhorse/core/typeclass/CNumeric.scala
+++ core/src/main/scala/scash/warhorse/core/typeclass/CNumeric.scala
@@ -1,57 +1,95 @@
 package scash.warhorse.core.typeclass
 
+import scash.warhorse.core._
+
 trait CNumeric[A] {
   def andMask: BigInt
   def num: A => BigInt
-  def apply: BigInt => A
+  def lift: BigInt => A
+  def min: A
+  def max: A
 
-  def sum[A1: CNumeric](a: A, a1: A1): A = apply(num(a) + CNumeric[A1].num(a1))
-  def sub[A1: CNumeric](a: A, a1: A1): A = apply(num(a) - CNumeric[A1].num(a1))
-  def mul[A1: CNumeric](a: A, a1: A1): A = apply(num(a) * CNumeric[A1].num(a1))
+  def sum[A1: CNumeric](a: A, a1: A1): A = lift(num(a) + CNumeric[A1].num(a1))
+  def sub[A1: CNumeric](a: A, a1: A1): A = lift(num(a) - CNumeric[A1].num(a1))
+  def mul[A1: CNumeric](a: A, a1: A1): A = lift(num(a) * CNumeric[A1].num(a1))
 
   def gt[A1: CNumeric](a: A, a1: A1): Boolean  = num(a) > CNumeric[A1].num(a1)
   def gte[A1: CNumeric](a: A, a1: A1): Boolean = num(a) >= CNumeric[A1].num(a1)
   def lt[A1: CNumeric](a: A, a1: A1): Boolean  = num(a) < CNumeric[A1].num(a1)
   def lte[A1: CNumeric](a: A, a1: A1): Boolean = num(a) <= CNumeric[A1].num(a1)
+  def eq[A1: CNumeric](a: A, a1: A1): Boolean  = num(a) == CNumeric[A1].num(a1)
 
-  def or[A1: CNumeric](a: A, a1: A1): A  = apply(num(a) | CNumeric[A1].num(a1))
-  def and[A1: CNumeric](a: A, a1: A1): A = apply(num(a) & CNumeric[A1].num(a1))
-  def xor[A1: CNumeric](a: A, a1: A1): A = apply(num(a) ^ CNumeric[A1].num(a1))
+  def or[A1: CNumeric](a: A, a1: A1): A  = lift(num(a) | CNumeric[A1].num(a1))
+  def and[A1: CNumeric](a: A, a1: A1): A = lift(num(a) & CNumeric[A1].num(a1))
+  def xor[A1: CNumeric](a: A, a1: A1): A = lift(num(a) ^ CNumeric[A1].num(a1))
 
-  def negative(a: A): A = apply(-num(a))
+  def negative(a: A): A = lift(-num(a))
 
-  def shiftL(a: A, a1: Int): A = apply((num(a) << a1) & andMask)
+  def shiftL(a: A, a1: Int): A = lift((num(a) << a1) & andMask)
 
   //this check is for weird behavior with the jvm and shift rights
   //https://stackoverflow.com/questions/47519140/bitwise-shift-right-with-long-not-equaling-zero/47519728#47519728
   def shiftR(a: A, a1: Int): A =
-    if (a1.toLong > 63) apply(0)
-    else apply(num(a) >> a1)
+    if (a1.toLong > 63) lift(0)
+    else lift(num(a) >> a1)
 }
 
 object CNumeric {
+
+  def consistencyLaw[A: CNumeric](a: A): Boolean =
+    CNumeric[A].lift(CNumeric[A].num(a)) === a
+
+  def rangeLaw[A: CNumeric](a: A): Boolean =
+    CNumeric[A].min <= CNumeric[A].max &&
+      a >= CNumeric[A].min && a <= CNumeric[A].max
+
+  def reflexiveLaw[A: CNumeric](a: A): Boolean =
+    (CNumeric[A].num(a) == CNumeric[A].num(a)) && (a === a)
+
+  def symmetryLaw[A: CNumeric, A1: CNumeric](a1: A, a2: A1) =
+    underylingIdentityLaw(a1, a2) && underylingIdentityLaw(a2, a1)
+
+  def additiveIdentityLaw[A: CNumeric](a: A): Boolean =
+    a + CNumeric[A].lift(BigInt(0)) === a
+
+  def subtractiveIdentityLaw[A: CNumeric](a: A): Boolean =
+    a - CNumeric[A].lift(BigInt(0)) === a
+
+  def multiplicativeIdentityLaw[A: CNumeric](a: A): Boolean =
+    a * CNumeric[A].lift(BigInt(1)) === a
+
+  def underylingIdentityLaw[A: CNumeric, A1: CNumeric](a1: A, a2: A1): Boolean =
+    !(a1 === a2) || (CNumeric[A].num(a1) == CNumeric[A1].num(a2))
+
+  def transitivityLaw[A1: CNumeric, A2: CNumeric, A3: CNumeric](a1: A1, a2: A2, a3: A3): Boolean =
+    !(a1 === a2 && a2 === a3) || (a1 === a3)
+
   def apply[A](implicit n: CNumeric[A]): CNumeric[A] = n
 
-  def apply[A](mask: BigInt)(n: A => BigInt, app: BigInt => A): CNumeric[A] = new CNumeric[A] {
-    def andMask = mask
-    def num     = n
-    def apply   = app
-  }
+  def apply[A](mask: BigInt, minimum: A, maximum: A)(n: A => BigInt, app: BigInt => A): CNumeric[A] =
+    new CNumeric[A] {
+      def andMask = mask
+      def num     = n
+      def lift    = app
+      def min     = minimum
+      def max     = maximum
+    }
 
 }
 
 trait CNumericSyntax {
   implicit class CNumericSyntaxOps[A: CNumeric](a: A) {
-    def +[A1: CNumeric](num: A1): A        = CNumeric[A].sum[A1](a, num)
-    def -[A1: CNumeric](num: A1): A        = CNumeric[A].sub(a, num)
-    def *[A1: CNumeric](num: A1): A        = CNumeric[A].mul(a, num)
-    def >[A1: CNumeric](num: A1): Boolean  = CNumeric[A].gt(a, num)
-    def >=[A1: CNumeric](num: A1): Boolean = CNumeric[A].gte(a, num)
-    def <[A1: CNumeric](num: A1): Boolean  = CNumeric[A].lt(a, num)
-    def <=[A1: CNumeric](num: A1): Boolean = CNumeric[A].lte(a, num)
-
-    def <<(num: Int): A = CNumeric[A].shiftL(a, num)
-    def >>(num: Int): A = CNumeric[A].shiftR(a, num)
+    def +[A1: CNumeric](num: A1): A         = CNumeric[A].sum[A1](a, num)
+    def -[A1: CNumeric](num: A1): A         = CNumeric[A].sub(a, num)
+    def *[A1: CNumeric](num: A1): A         = CNumeric[A].mul(a, num)
+    def >[A1: CNumeric](num: A1): Boolean   = CNumeric[A].gt(a, num)
+    def >=[A1: CNumeric](num: A1): Boolean  = CNumeric[A].gte(a, num)
+    def <[A1: CNumeric](num: A1): Boolean   = CNumeric[A].lt(a, num)
+    def <=[A1: CNumeric](num: A1): Boolean  = CNumeric[A].lte(a, num)
+    def ===[A1: CNumeric](num: A1): Boolean = CNumeric[A].eq(a, num)
+    def !==[A1: CNumeric](num: A1): Boolean = CNumeric[A].eq(a, num)
+    def <<(num: Int): A                     = CNumeric[A].shiftL(a, num)
+    def >>(num: Int): A                     = CNumeric[A].shiftR(a, num)
 
     def |(num: A): A = CNumeric[A].or(a, num)
     def &(num: A): A = CNumeric[A].and(a, num)
