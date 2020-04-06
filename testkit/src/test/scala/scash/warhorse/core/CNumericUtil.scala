89045b5829c8a97906f3670c46850000c1996baf
sken <sken77@pm.me>
2020-04-05 21:30:50 -0700

    refactor tests

---
+++ testkit/src/test/scala/scash/warhorse/core/CNumericUtil.scala
@@ -0,0 +1,68 @@
+package scash.warhorse.core
+
+import scash.warhorse.core.typeclass.CNumeric
+import scodec.Codec
+import zio.test.Assertion.Render.param
+import zio.test.Assertion.{ equalTo, isNone, isTrue }
+import zio.test.{ assert, Assertion }
+
+import scala.util.Try
+
+object CNumericUtil {
+  def equalTo_[A: CNumeric](expected: A): Assertion[A] =
+    Assertion.assertion("equalTo")(param(expected))(_ === expected)
+
+  def shiftL[A: CNumeric](u: A, i: Int) = {
+    val Cnum     = CNumeric[A]
+    val expected = (Cnum.num(u) << i) & Cnum.andMask
+    if (Cnum.max > expected) assert(u << i)(equalTo(Cnum.lift(expected)))
+    else assert(Try(u << i).isFailure)(isTrue)
+  }
+
+  def shiftR[A: CNumeric](u: A, i: Int, size: Int) = {
+    val Cnum = CNumeric[A]
+    val r    = u >> i
+    val expected =
+      if (i >= size) Cnum.min
+      else Cnum.lift(Cnum.num(u) >> i)
+    assert(r)(equalTo_(expected))
+  }
+
+  def sum[A: CNumeric](u1: A, u2: A) = {
+    val expected = CNumeric[A].num(u1) + CNumeric[A].num(u2)
+    if (expected > CNumeric[A].num(CNumeric[A].max)) assert(Try(u1 + u2).isFailure)(isTrue)
+    else assert(u1 + u2)(equalTo_(CNumeric[A].lift(expected)))
+  }
+
+  def substract[A: CNumeric](u1: A, u2: A) = {
+    val expected = CNumeric[A].num(u1) - CNumeric[A].num(u2)
+    if (expected < CNumeric[A].num(CNumeric[A].min)) assert(Try(u1 - u2).isFailure)(isTrue)
+    else assert(u1 - u2)(equalTo_(CNumeric[A].lift(expected)))
+  }
+
+  def mult[A: CNumeric](u1: A, u2: A) = {
+    val expected = CNumeric[A].num(u1) * CNumeric[A].num(u2)
+    if (expected > CNumeric[A].num(CNumeric[A].max)) assert(Try(u1 * u2).isFailure)(isTrue)
+    else assert(u1 * u2)(equalTo_(CNumeric[A].lift(expected)))
+  }
+
+  def bitwiseInclusive[A: CNumeric](u1: A, u2: A) =
+    assert(u1 | u2)(equalTo_(CNumeric[A].lift(CNumeric[A].num(u1) | CNumeric[A].num(u2))))
+
+  def bitwiseExclusive[A: CNumeric](u1: A, u2: A) =
+    assert(u1 ^ u2)(equalTo_(CNumeric[A].lift(CNumeric[A].num(u1) ^ CNumeric[A].num(u2))))
+
+  def bitwiseAnd[A: CNumeric](u1: A, u2: A) =
+    assert(u1 & u2)(equalTo_(CNumeric[A].lift(CNumeric[A].num(u1) & CNumeric[A].num(u2))))
+
+  def symmetry[A: Codec: CNumeric](u: A) = assert(u.bytes.decode[A])(equalTo_(u))
+
+  def outofBounds[A: CNumeric] =
+    assert(Try(CNumeric[A].max + CNumeric[A].lift(BigInt(1))).toOption)(isNone) &&
+      assert(Try(CNumeric[A].min - CNumeric[A].lift(BigInt(1))).toOption)(isNone)
+
+  def testBounds[A: CNumeric](min: BigInt, max: BigInt) =
+    assert(CNumeric[A].num(CNumeric[A].min))(equalTo(min)) &&
+      assert(CNumeric[A].num(CNumeric[A].max))(equalTo(max))
+
+}
---
+++ testkit/src/test/scala/scash/warhorse/core/number/Uint32Spec.scala
@@ -0,0 +1,43 @@
+package scash.warhorse.core.number
+
+import scash.warhorse.gen
+import scash.warhorse.core.CNumericUtil._
+import scash.warhorse.core._
+
+import scodec.bits.ByteVector
+
+import scala.util.Try
+
+import zio.test.Assertion._
+import zio.test._
+import zio.test.Assertion.equalTo
+
+object Uint32Spec extends DefaultRunnableSpec {
+  val spec = suite("Uint32")(
+    suite("CNumeric")(
+      testM("shiftL")(check(gen.uint32, Gen.int(0, 32))(shiftL)),
+      testM("shiftR")(check(gen.uint32, Gen.int(0, 100))(shiftR(_, _, 64))),
+      testM("sum")(check(gen.uint32, gen.uint32)(sum)),
+      testM("substract")(check(gen.uint32, gen.uint32)(substract)),
+      testM("multiply")(check(gen.uint32, gen.uint32)(mult)),
+      testM("bitwiseInclusive |")(check(gen.uint32, gen.uint32)(bitwiseInclusive)),
+      testM("bitwiseExclusive ^")(check(gen.uint32, gen.uint32)(bitwiseExclusive)),
+      testM("bitwiseAnd &")(check(gen.uint32, gen.uint32)(bitwiseAnd)),
+      test("out of bounds")(outofBounds[Uint32]),
+      test("test bounds")(testBounds[Uint32](BigInt(0), BigInt(4294967295L)))
+    ),
+    suite("Serde")(
+      testM("symmetry")(check(gen.uint32)(symmetry)),
+      test("0")(assert(ByteVector(0, 0, 0, 0).decode[Uint32])(equalTo_(Uint32.min))),
+      test("1")(assert(ByteVector(1, 0, 0, 0).decode[Uint32])(equalTo_(Uint32.one))),
+      test("16777216")(assert(ByteVector(0, 0, 0, 1).decode[Uint32])(equalTo_(Uint32(16777216)))),
+      test("65536")(assert(ByteVector(0, 0, 1, 0).decode[Uint32])(equalTo_(Uint32(65536)))),
+      test("0xFF")(assert(ByteVector(0xFF, 0, 0, 0).decode[Uint32])(equalTo_(Uint32(255)))),
+      test("max to hex")(assert(Uint32.max.hex)(equalTo("ffffffff"))),
+      test("min to hex")(assert(Uint32.min.hex)(equalTo("00000000"))),
+      test("0xffffffff == Uint32.max")(assert(ByteVector(0xff, 0xff, 0xff, 0xff).decode[Uint32])(equalTo_(Uint32.max))),
+      test("too large bytevector 0")(assert(Try(ByteVector(0, 0, 0, 0, 0).decodeExactly[Uint32]).toOption)(isNone)),
+      test("too large bytevector 1")(assert(Try(ByteVector(1, 1, 1, 1, 1).decodeExactly[Uint32]).toOption)(isNone))
+    )
+  )
+}
