2bce0e97fc4bb4713ac5b4752b91c0cc603b8958
sken <22631338+sken77@users.noreply.github.com>
2020-04-08 14:19:16 -0700

    fix out of bounds errors with extremely large numbers (#7)
    * fix out of bounds errors with extremely large numbers
    
    * fix debugging leftovers

--- core/src/main/scala/scash/warhorse/core/number/Uint8.scala
+++ core/src/main/scala/scash/warhorse/core/number/Uint8.scala
@@ -13,6 +13,8 @@ object Uint8 {
 
   def apply(i: Int): Uint8 = new Uint8(verify(i)(min, max))
 
+  def apply(n: BigInt): Uint8 = new Uint8(verify(n)(min, max).toInt)
+
   def safe(i: Int): Option[Uint8] = Try(apply(i)).toOption
 
   val min = new Uint8(0)
@@ -22,6 +24,6 @@ object Uint8 {
   implicit val uint8Codec: Codec[Uint8] = uint8L.xmap(apply(_), _.num)
 
   implicit val uint8Numeric: CNumeric[Uint8] =
-    CNumeric[Uint8](0xFF, min, max)(_.num, n => apply(n.toInt))
+    CNumeric[Uint8](0xFF, min, max)(_.num, apply(_))
 
 }
