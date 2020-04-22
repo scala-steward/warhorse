befc6a888698c36d918fd00e80c679a638a141b1
Scala Steward <me@scala-steward.org>
2020-04-22 22:40:24 +0200

    Update zio-test, zio-test-sbt to 1.0.0-RC18-2+167-cde926fd-SNAPSHOT

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ object Deps {
     val scodec     = "1.11.7"
     val scodecbits = "1.1.14"
     val sttp       = "2.0.7"
-    val zio        = "1.0.0-RC18-2+147-6dcf6568-SNAPSHOT"
+    val zio        = "1.0.0-RC18-2+167-cde926fd-SNAPSHOT"
   }
 
   object Libs {
