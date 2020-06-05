64b08d9bc62856ecfc90ac25751cec749b73f4f0
Scala Steward <me@scala-steward.org>
2020-06-06 01:00:59 +0200

    Update async-http-client-backend-zio, ... to 2.1.5

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ object Deps {
     val circe        = "0.13.0"
     val scodec       = "1.11.7"
     val scodecbits   = "1.1.15"
-    val sttp         = "2.1.4"
+    val sttp         = "2.1.5"
     val zio          = "1.0.0-RC19-2"
     val ziosecp256k1 = "0.1.5"
   }
