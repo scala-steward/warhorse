98d29355ce2b1b9b90721fdd45a228a029e83811
sken <22631338+sken77@users.noreply.github.com>
2020-06-22 17:54:53 -0500

    simplify hash codec (#90)

--- core/src/main/scala/scash/warhorse/core/crypto/hash/DoubleSha256.scala
+++ core/src/main/scala/scash/warhorse/core/crypto/hash/DoubleSha256.scala
@@ -1,9 +1,11 @@
 package scash.warhorse.core.crypto.hash
 
-import org.bouncycastle.crypto.Digest
-import scash.warhorse.core.crypto.hash.Sha256.sha256Serde
 import scash.warhorse.core.typeclass.{ Hasher, Serde }
+
 import scodec.bits.ByteVector
+import scodec.codecs.bytes
+
+import org.bouncycastle.crypto.Digest
 
 case class DoubleSha256(private[crypto] val b: ByteVector)
 case class DoubleSha256B(private[crypto] val b: ByteVector)
@@ -18,8 +20,7 @@ object DoubleSha256B {
 object DoubleSha256 {
   def toBigEndian(dsha256: DoubleSha256): DoubleSha256B = DoubleSha256B(dsha256.b.reverse)
 
-  implicit val doubleSha256Serde: Serde[DoubleSha256] =
-    sha256Serde.xmap(b => DoubleSha256(b.b), a => Sha256(a.b))
+  implicit val doubleSha256Serde: Serde[DoubleSha256] = Serde(bytes(32).as[DoubleSha256])
 
   implicit val doubleSha256hash = new Hasher[DoubleSha256] {
     def cons(b: ByteVector): DoubleSha256 = DoubleSha256(Hasher[Sha256].hash(b).b)
--- core/src/main/scala/scash/warhorse/core/crypto/hash/RipeMd160.scala
+++ core/src/main/scala/scash/warhorse/core/crypto/hash/RipeMd160.scala
@@ -2,27 +2,22 @@ package scash.warhorse.core.crypto.hash
 
 import org.bouncycastle.crypto.Digest
 import org.bouncycastle.crypto.digests.RIPEMD160Digest
-import scash.warhorse.Err
-import scash.warhorse.Result.{ Failure, Successful }
+
 import scash.warhorse.core.typeclass.{ Hasher, Serde }
-import scodec.DecodeResult
+
+import scodec.codecs.bytes
 import scodec.bits.ByteVector
 
 protected[warhorse] case class RipeMd160(private[crypto] val b: ByteVector)
 
 object RipeMd160 {
 
-  implicit val ripeMd160Serde: Serde[RipeMd160] = Serde[RipeMd160](
-    (a: RipeMd160) => Successful(a.b),
-    (bytes: ByteVector) =>
-      if (bytes.size == bsize) Successful(DecodeResult(RipeMd160(bytes.take(bsize)), bytes.drop(bsize).bits))
-      else Failure(Err.BoundsError("RipeMD160", s"needs $bsize bytes", s"${bytes.size} bytes"))
-  )
+  implicit val ripeMd160Serde: Serde[RipeMd160] =
+    Serde[RipeMd160](bytes(20).as[RipeMd160])
 
   implicit val ripemd160hash = new Hasher[RipeMd160] {
     def cons(b: ByteVector): RipeMd160 = RipeMd160(b)
     def hasher: Digest                 = new RIPEMD160Digest
   }
 
-  private val bsize = 20L
 }
